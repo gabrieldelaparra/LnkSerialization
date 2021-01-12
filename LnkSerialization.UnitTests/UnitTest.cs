b09ef818122eaa0568e3139bec3fe3b3795a4df0
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2021-01-12 17:33:06 +0100

    Add tests to validate paths;

--- LnkSerialization.UnitTests/UnitTest.cs
+++ LnkSerialization.UnitTests/UnitTest.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using Wororo.Utilities;
 using Xunit;
 
 namespace LnkSerialization.UnitTests
@@ -15,9 +16,33 @@ namespace LnkSerialization.UnitTests
         public void TestSerializeAllLinksAsShortcuts()
         {
             var lnkFiles = Directory.GetFiles("Resources/", "*.lnk");
-            foreach (var lnkFile in lnkFiles) {
+            foreach (var lnkFile in lnkFiles)
+            {
                 Core.LnkSerialization.SerializeAsShortcutJson(lnkFile);
             }
+        }
+
+        [Fact]
+        public void TestSerializeNonWorkingLinkAsShortcuts()
+        {
+            Core.LnkSerialization.SerializeAsShortcutJson("Resources/non-working.lnk");
+        }
+
+        [Fact]
+        public void TestSerializeAndDeserializeNonWorkingLinkAsShortcuts()
+        {
+            Core.LnkSerialization.SerializeAsShortcutJson("Resources/non-working.lnk");
+            var lnkModel = new Core.LinkModel("Resources/non-working.lnk");
+            JsonSerialization.SerializeJson(lnkModel, "non-working-AsLnkModel.json");
+            var outputFolder = "FromLnkModel/";
+            outputFolder.CreatePathIfNotExists();
+            lnkModel.ToLinkFile(outputFolder);
+        }
+
+        [Fact]
+        public void TestSerializeWorkingLinkAsShortcuts()
+        {
+            Core.LnkSerialization.SerializeAsShortcutJson("Resources/working.lnk");
         }
 
         [Fact]
@@ -29,15 +54,23 @@ namespace LnkSerialization.UnitTests
         [Fact]
         public void TestSerializeMyProductionLinks()
         {
-            Core.LnkSerialization.SerializeLinkFolder(@"C:\Dev\settings\shortcuts\",
-                @"C:\Dev\settings\shortcuts\shorcuts.json");
+            Core.LnkSerialization.SerializeLinkFolder(@"C:\Apps\settings\shortcuts\",
+                @"C:\Apps\settings\shortcuts.json");
+        }
+
+        [Fact]
+        public void TestDeserializeMyProductionLinks()
+        {
+            Core.LnkSerialization.DeserializeLinksToFolder(@"C:\Apps\settings\shortcuts.json",
+                @"C:\Apps\settings\output\");
         }
 
         [Fact]
         public void TestSerializeMyProductionLinksAsShortcuts()
         {
-            var lnkFiles = Directory.GetFiles(@"C:\Dev\settings\shortcuts\", "*.lnk");
-            foreach (var lnkFile in lnkFiles) {
+            var lnkFiles = Directory.GetFiles(@"C:\Apps\settings\shortcuts\", "*.lnk");
+            foreach (var lnkFile in lnkFiles)
+            {
                 Core.LnkSerialization.SerializeAsShortcutJson(lnkFile);
             }
         }
